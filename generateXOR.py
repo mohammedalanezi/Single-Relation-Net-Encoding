ee85f81c3f39c5d6bd952dc510ff6ef4904b07a2
Mohammed Al-Anezi <228255292+mohammedalanezi@users.noreply.github.com>
2025-09-19 17:24:33 -0400

    XOR encoding works for any k-net(n) and any relation type

--- generateXOR.py
+++ generateXOR.py
@@ -1,8 +1,6 @@
 # input: variable count, cutting size, cutting type
 # output: clauses required for xor chain and variable count
 
-# MODIFIED VERSION OF SCRIPT, USED ONLY FOR 4-NET(6)
-
 import os
 import subprocess
 import sys
@@ -13,43 +11,47 @@ clauses_path = os.path.join(script_dir, "xor_clauses.cnf")
 
 clauses = []
 
-order = 36
+relation_type = [4,4,4,4]
+net_classes = 4
+net_order = 10
+
+row_length = net_classes * net_order
+col_length = net_order * net_order
+
+order = sum(relation_type)
 cutting_size = 4
 cutting_type = "Linear"
 
-solutionsOnly = True
-
 overwrite_variables = []
-
-# 2-net(6) with 6^2 = 36 order, we duplicate it 8 times to fill out all required columns as seen in updateClauses' inner workings. 
-# if all constraints we get 0 then it means linear depedence exists, otherwise no dependence relation exists. Its shown in stinson's paper that the 4th linear dependence relation doesnt exist so we expect 1.
-# what we are encoding is the search for linear independence (1) which would prove that 4-net(6) exists. We already know, from stinson's paper, that this will be UNSAT
-for i in range(0, 6 ** 2):
-    overwrite_variables.append(i * 4 * 6 + 1)
+variable_offset = row_length * col_length
 
 auxiliary_variables = 0
 variable_list = [] 
 chains = []
+    
+for i in range(len(relation_type)):
+    for j in range(relation_type[i]):
+        overwrite_variables.append(i * net_order + j + 1) 
 
 def updateClauses(): # update the clauses.cnf file
     with open(clauses_path, "w") as f:
-        for clause in clauses:
-            for i in range(4): # iterate for r, c, s, t
-                for j in range(2): # iterate for index x_0, x_1; where x is r, c, s, or t
-                    mapped = []
-                    for var in clause:
-                        isNegative = var < 0
-                        if abs(var) <= order:  # main variable
-                            new_var = overwrite_variables[abs(var) - 1] + i * 6 + j # offset by index and column selected
-                            if isNegative:
-                                new_var *= -1
-                            mapped.append(str(new_var))
-                        else:  # auxiliary variable
-                            new_var = abs(var) + 864 - 36 + auxiliary_variables * (i * 2 + j) # offset such that auxiliary_variables are not reused
-                            if isNegative:
-                                new_var *= -1
-                            mapped.append(str(new_var))
-                    f.write(" ".join(mapped) + "\n") # + " 0\n" not needed since we will add that in our generate.py script
+        for i in range(col_length): # iterate for each row
+            for clause in clauses:
+                mapped = []
+                for var in clause:
+                    isNegative = var < 0
+                    if abs(var) <= order:  # main variable
+                        new_var = overwrite_variables[abs(var) - 1] + row_length * i # offset by index
+                        if isNegative:
+                            new_var *= -1
+                        mapped.append(str(new_var))
+                    else:  # auxiliary variable
+                        new_var = abs(var) + variable_offset - order + auxiliary_variables * i # offset such that auxiliary_variables are not reused
+                        if isNegative:
+                            new_var *= -1
+                        mapped.append(str(new_var))
+                f.write(" ".join(mapped) + "\n") # + " 0\n" not needed since we will add that in our generate.py script
+            # f.write("\n")
 
 def getCombinations(totalList, array, n, currentRemovals): # n >= 0, generate all possible combinations from n choices
     if n == 0:
@@ -75,8 +77,7 @@ def add_xor_clauses(chain): # create XOR clauses for given chain, should add 2^(
 def recursive_xor(): # create XOR chains
     global auxiliary_variables
     if len(variable_list) <= cutting_size:
-        if solutionsOnly == False:
-            variable_list[0] = -variable_list[0]
+        variable_list[0] = -variable_list[0]
         chains.append(variable_list)
     else:
         tempList = []
@@ -98,14 +99,15 @@ for i in range(order):
     variable_list.append(i + 1)
 
 recursive_xor()
-print(f"Input parameters: order {order}, cutting size {cutting_size}, cutting style \"{cutting_type}\", only solutions \"{solutionsOnly}\".")
+print(f"Input parameters: order {order}, cutting size {cutting_size}, cutting style \"{cutting_type}\".")
+print("Variables Mapped to:", overwrite_variables)
 print("\nXOR Chains:",chains)
 for i in range(len(chains)):
     add_xor_clauses(chains[i])
 
 updateClauses()
 
-print(f"\nGenerated {len(clauses)} XOR clauses by introducing {auxiliary_variables} auxiliary variable(s).")
+print(f"\nGenerated {len(clauses)} (Total: {len(clauses) * col_length}) XOR clauses by introducing {auxiliary_variables} (Total: {auxiliary_variables * col_length}) auxiliary variable(s).")
 print(f"Wrote clauses to: {clauses_path}")
 
 # cd /mnt/g/Code/sat\ solver\ stuff/stinson\ xor
